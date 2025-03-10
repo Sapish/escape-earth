978d3396519078f2d62332b169748b283f673be4
Oskar <oskardb@uia.no>
2025-03-10 19:54:51 +0100

    finished task 1

--- main.mjs
+++ main.mjs
@@ -6,25 +6,26 @@ const playerId = "oskardb@uia.no";
 const GAME_API = "https://spacescavanger.onrender.com/";
 
 (async function() {
-    try {
     const startUrl = `${GAME_API}start?player=${encodeURIComponent(playerId)}`;
     const startResponse = await fetch(startUrl);
     const startData = await startResponse.json();
     console.log("Challenge started:", startData);
 
-    const sunUrl = `${SOLAIRE_API}bodies/sun`;
-    const sunResponse = await fetch(sunUrl);
+    const sunResponse = await fetch(`${SOLAIRE_API}bodies/soleil`);
     const sunData = await sunResponse.json();
+    console.log("Sun Data: ", sunData);
 
     const accessPin = sunData.equaRadius - sunData.meanRadius;
     console.log("Access Pin:", accessPin);
 
-    const submitUrl = `${GAME_API}submit?player=${encodeURIComponent(playerId)}&pin=${accessPin}`;
+    const answer = accessPin;
 
-    const submitResponse = await fetch(submitUrl);
-    const submitData = await submitResponse.json();
-    console.log("Submission result: ", submitData);
-} catch (error) {
-    console.log("Something seems to have went wrong: ", error.message);
-}
+    const answerFeedback = await fetch(`${GAME_API}answer`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ answer, player: playerId })
+    });
+    const answerResult = await answerFeedback.json();
+    console.log("Task 1 Result: ", answerResult);
 })();
+
