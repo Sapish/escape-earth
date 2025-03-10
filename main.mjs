9d7adba251eeae817234ced25efd0da525be417a
Oskar <oskardb@uia.no>
2025-03-10 17:56:31 +0100

    distress message

--- main.mjs
+++ main.mjs
@@ -1,1 +1,13 @@
 
+import fetch from 'node-fetch';
+
+const SOLAIRE_API = "https://api.le-systeme-solaire.net/rest/";
+const playerId = "oskardb@uia.no";
+const GAME_API = "https://spacescavanger.onrender.com/";
+
+(async function() {
+    const startUrl = `${GAME_API}start?player=${encodeURIComponent(playerId)}`;
+    const startResponse = await fetch(startUrl);
+    const startData = await startResponse.json();
+    console.log("Challenge started:", startData);
+})();
